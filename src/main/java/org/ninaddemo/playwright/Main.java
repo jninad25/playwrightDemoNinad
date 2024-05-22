3499c86bb006beec17b7231f28fa4414670a702c
= <=>
2024-05-22 20:53:33 +0530

    First Commit to main

---
+++ src/main/java/org/ninaddemo/playwright/Main.java
@@ -0,0 +1,8 @@
+package org.ninaddemo.playwright;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("Play my test case");
+  }
+}
